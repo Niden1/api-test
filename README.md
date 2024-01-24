963d91756ab8bc6c5e420d1de6eca0e8a276218e
Niden <80420845+Niden1@users.noreply.github.com>
2024-01-24 14:38:44 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# api-test
+# API-Example
+
+An example of how to create an API with Node.js
+
+CyberDoge is an API that can be mostly used for Discord. [here](https://api.cyberdoge.ga/).
+
+## Support
+
+ - [Discord](https://discord.homes/cyberdoge-support)
+ - [GitHub](https://github.com/my-universal-network)
+
+## Contact
+
+For any issues, you can contact **Niden#6677** via Discord or ask for help in the [Support Server](https://discord.gg/9UpwnJr72w).
+
+## License
+
+Copyright (C) 2021 Niden and contributors</br>
+See [`LICENSE`](https://github.com/my-universal-network/API-Example/blob/main/LICENSE) for more information.
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const rateLimit = require("express-rate-limit")
+const PORT = process.env.PORT || 8000
+const JSONdb = require('simple-json-db');
+const db = new JSONdb(`${__dirname}/database.json`)
+
+// Rate limiter
+const limiter = rateLimit({
+    windowMs: 100 * 60 * 100, // 10 minutes
+    max: 100, // 100 requests max every 10 minutes
+    message: "You are sending too many requests. Please try again later."
+})
+
+//API Requests
+app.get('/*', (req, res, next) => {
+    db.set('requests', (db.get('requests') + 1))
+    console.log(`${req.headers['x-forwarded-for']}    ${req.connection.remoteAddress}     ${req.url}`)
+    next()
+})
+
+//Endpoints
+app.get('/hello', (req, res) => {
+    res.status(200).send({
+      message: "Hello World!"
+    });
+});
+
+// Listen on port
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+app.listen(3000, () => {
+    console.log('API Online');
+});
